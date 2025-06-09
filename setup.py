281de6a279bc4f9f34efc400db34c1b5dbc11e06
jngaiii <jngai_@berkeley.edu>
2025-06-09 21:12:44 +0800

    adding egg-info file

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TestAutoABSA",
+    name="OCFO-ETL",
     version="0.1",
     packages=find_packages(),  # Automatically detects your package folders
     install_requires=[
         "numpy",
         "pandas", 
         "scikit-learn", 
+        "spacy", 
         "google-api-python-client", 
         "google-auth-httplib2", 
         "google-auth-oauthlib"
