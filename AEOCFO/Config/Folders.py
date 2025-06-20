fd1498ba91f18ab8a431e0aae6ac6157cd11878d
jngaiii <jngai_@berkeley.edu>
2025-06-20 14:50:55 -0700

    updated debugging scripts

--- AEOCFO/Config/Folders.py
+++ AEOCFO/Config/Folders.py
@@ -1,20 +1,24 @@
-ABSA_INPUT_FOLDER_ID = "1nlYOz8brWYgF3aKsgzpjZFIy1MmmEVxQ"
-ABSA_OUTPUT_FOLDER_ID = "1ELodPGvuV7UZRhTl1x4Phh0PzMDescyG"
-OASIS_INPUT_FOLDER_ID = "1WwgSJoEyz1RKsdSJop3oSDlZ5LqZ_ZKz"
-OASIS_OUTPUT_FOLDER_ID = "1r-1xZMnqYkp1BS59Mnq4Gs3z0yKeUAIL"
-CONTINGENCY_INPUT_FOLDER_ID = "1pNoRbJgNeVacqr4RzTbYCi1DVZ1qiwzP"
-CONTINGENCY_OUTPUT_FOLDER_ID = "1w1g7xvVNKSX2RgHVhuN9UsDB_HLUkLOj"
-
-OVERWRITE_FOLDER_ID = '1j9cbFLhxlP4CeH1nfdVlHRNbkpXEwyzi'
+id_dict = {
+    'ABSA_INPUT_FOLDER_ID' : "1nlYOz8brWYgF3aKsgzpjZFIy1MmmEVxQ", 
+    'ABSA_OUTPUT_FOLDER_ID' : "1ELodPGvuV7UZRhTl1x4Phh0PzMDescyG", 
+    'OASIS_INPUT_FOLDER_ID' : "1WwgSJoEyz1RKsdSJop3oSDlZ5LqZ_ZKz", 
+    'OASIS_OUTPUT_FOLDER_ID' : "1r-1xZMnqYkp1BS59Mnq4Gs3z0yKeUAIL", 
+    'CONTINGENCY_INPUT_FOLDER_ID' : "1pNoRbJgNeVacqr4RzTbYCi1DVZ1qiwzP", 
+    'CONTINGENCY_OUTPUT_FOLDER_ID' : "1w1g7xvVNKSX2RgHVhuN9UsDB_HLUkLOj", 
+    'OVERWRITE_FOLDER_ID' : '1j9cbFLhxlP4CeH1nfdVlHRNbkpXEwyzi'
+}
 
 def get_overwrite_folder_id():
-    return OVERWRITE_FOLDER_ID
+    return id_dict['OVERWRITE_FOLDER_ID']
 
 def get_absa_folder_id():
-    return ABSA_INPUT_FOLDER_ID, ABSA_OUTPUT_FOLDER_ID
+    return id_dict['ABSA_INPUT_FOLDER_ID'], id_dict['ABSA_OUTPUT_FOLDER_ID']
 
 def get_oasis_folder_id():
-    return OASIS_INPUT_FOLDER_ID, OASIS_OUTPUT_FOLDER_ID
+    return id_dict['OASIS_INPUT_FOLDER_ID'], id_dict['OASIS_OUTPUT_FOLDER_ID']
 
 def get_contingency_folder_id():
-    return CONTINGENCY_INPUT_FOLDER_ID, CONTINGENCY_OUTPUT_FOLDER_ID
+    return id_dict['CONTINGENCY_INPUT_FOLDER_ID'], id_dict['CONTINGENCY_OUTPUT_FOLDER_ID']
+
+def get_all_ids():
+    return id_dict
--- debugs/check_permissions.py
+++ debugs/check_permissions.py
@@ -1,14 +1,5 @@
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
-
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-API_NAME = "drive"
-API_VERSION = "v3"
-SERVICE_ACCOUNT_FILE = "credentials.json"  # Store credentials securely!
-
-def authenticate_drive():
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    return build(API_NAME, API_VERSION, credentials=creds)
+from AEOCFO.Config.Folders import *
+from AEOCFO.Utility.Authenticators import authenticate_drive
 
 #DEBUG
 # def general_CSV_search() -> list:
@@ -42,14 +33,9 @@ if __name__ == "__main__":
     print(f"Service: {service}") 
     about = service.about().get(fields="user").execute()
     print(f"Connected as: {about['user']['emailAddress']}")
-    
-    ABSA_INPUT_FOLDER_ID = "1nlYOz8brWYgF3aKsgzpjZFIy1MmmEVxQ"
-    ABSA_OUTPUT_FOLDER_ID = "1ELodPGvuV7UZRhTl1x4Phh0PzMDescyG"
 
     #DEBUG
-    input_permissions = service.permissions().list(fileId=ABSA_INPUT_FOLDER_ID).execute()
-    output_permissions = service.permissions().list(fileId=ABSA_OUTPUT_FOLDER_ID).execute()
-    print(f"Permission to input folder: {input_permissions}")
-    print(f"Permission to output folder: {output_permissions}")
+    for key, value in get_all_ids().items():
+        print(f"Permission to {key} folder: Accessible!")
 
     
