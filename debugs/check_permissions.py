6f42ec3ed9fa28b81540e017da4717bc9cce5ca2
jngaiii <jngai_@berkeley.edu>
2025-06-22 07:40:46 -0700

    updated permission check python and yaml scripts

--- debugs/check_permissions.py
+++ debugs/check_permissions.py
@@ -1,41 +1,17 @@
 from AEOCFO.Config.Folders import *
 from AEOCFO.Utility.Authenticators import authenticate_drive
 
-#DEBUG
-# def general_CSV_search() -> list:
-#     try: 
-#         service = authenticate_drive()
-#         query = "name contains '.csv' and name contains 'ABSA' and name contains 'RF'"
-#         page_token = None
-#         files = []
-#         while True:
-#             # pylint: disable=maybe-no-member
-#             response = (service.files().list(
-#                 q=query, spaces="drive", fields="nextPageToken, files(id, name)", pageToken=page_token,
-#                 ).execute())
-#             for file in response.get("files", []):
-#                 # Process change
-#                 print(f'Found file with name: {file.get("name")}, ID: {file.get("id")}')
-#             files.extend(response.get("files", []))
-#             page_token = response.get("nextPageToken", None)
-#             if page_token is None:
-#                 break
-
-#     except HttpError as error:
-#         print(f"An error occurred: {error}")
-#         files = None
-
-#     return files
-
 if __name__ == "__main__":
-    #DEBUG
     service = authenticate_drive()
-    print(f"Service: {service}") 
     about = service.about().get(fields="user").execute()
     print(f"Connected as: {about['user']['emailAddress']}")
 
-    #DEBUG
-    for key, value in get_all_ids().items():
-        print(f"Permission to {key} folder: Accessible!")
+    folder_ids = get_all_ids()
+    for key, folder_id in folder_ids.items():
+        try:
+            folder = service.files().get(fileId=folder_id, fields='id, name').execute()
+            print(f"Permission to {key} folder: Accessible! (Folder name: {folder['name']})")
+        except Exception as e:
+            print(f"Permission to {key} folder: DENIED or ERROR! ({e})")
 
     
