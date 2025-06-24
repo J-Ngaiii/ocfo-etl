6d3a345bd46c4b992e36bf0de053ff26e295ee4a
jngaiii <jngai_@berkeley.edu>
2025-06-24 12:00:29 -0700

    minor changes to check_pathing.py

--- debugs/check_pathing.py
+++ debugs/check_pathing.py
@@ -4,14 +4,15 @@ import sys
 import sys
 print(f"Sys path: {sys.path}")
 
-from AEOCFO.Transform import ABSA_Processor, Agenda_Processor, OASIS_Abridged
+from AEOCFO.Transform import ABSA_Processor, Agenda_Processor, OASIS_Abridged, FR_ProcessorV2, process_weekly_pipeline
 import AEOCFO.Utility.Cleaning as cl
 import AEOCFO.Utility.Utils as ut
 
 print(f"Utils Func: {ut.heading_finder}")
 print(f"Cleaning Func: {cl.is_type}")
-print(f"{ABSA_Processor.__name__}: imported successfully!")
-print(f"{Agenda_Processor.__name__}: imported successfully!")
-print(f"{OASIS_Abridged.__name__}: imported successfully!")
+
+functions = [ABSA_Processor, Agenda_Processor, OASIS_Abridged, FR_ProcessorV2, process_weekly_pipeline]
+for func in functions:
+    print(f"{func.__name__}: imported successfully!")
 
 print("All print statements completed, pathing functional!")
